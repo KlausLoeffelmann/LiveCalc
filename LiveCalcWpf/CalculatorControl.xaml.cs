98114451becae51221a134ba387c7177621adc10
Peter Spada <spadapet@hotmail.com>
2025-07-30 14:42:15 -0700

    Copilot created CalculatorControl

---
+++ LiveCalcWpf/CalculatorControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CalcWpf
+{
+    public partial class CalculatorControl : UserControl
+    {
+        public CalculatorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
